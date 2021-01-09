93fb1794cbafc33199a618ebadf68d81b80ca483
averycordle <77205707+averycordle@users.noreply.github.com>
2021-01-09 13:17:56 -0500

    Added Assignment 1

---
+++ assignments/assignment1/assignment1.py
@@ -0,0 +1,4 @@
+#Author: Avery Cordle (my first name is Martha, but I go by Avery. Feel free to call me either.)
+print("Avery Cordle")
+print("An interesting fact about me would be I ran 3 half-marathons last year.")
+print("My favorite thing to do is a tie between running, painting, and cooking for my family.")
