e8a572840150f88109531522d2e029a9302152e0
averycordle <77205707+averycordle@users.noreply.github.com>
2021-01-14 09:39:49 -0500

    Add files via upload

---
+++ exercises/jan12/jan14/string_input.py
@@ -0,0 +1,6 @@
+print("Welcome to my program")
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+
+print("hi " + firstName + " " + lastName + "!")
---
+++ exercises/jan12/jan14/strings_vars.py
@@ -0,0 +1,22 @@
+#Author: Avery Cordle
+#learnng strings and variables
+
+firstName= "Avery"
+middleName= "Martha"
+lastName= "Cordle"
+
+
+#print(firstName + " " + lastName)
+
+#print(f"{firstName} {lastName}")
+
+print("Welcome " + firstName + " " + middleName + " " + lastName)
+
+print("first line\nlast line")
+
+print("""
+I lve
+multiple
+lines""")
+
+
